07858a567296a11121c41cd49554f20d00412ec5
Hamzah Melibary <mr.hamzh@hotmail.com>
2023-05-07 03:56:36 +0300

    put index and php out of folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["/**/*.{html,js}"],
     theme: {
       extend: {
         colors: {
