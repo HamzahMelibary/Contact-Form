d5667405294e35c7e70b875f4cd52d7087299268
Hamzah Melibary <mr.hamzh@hotmail.com>
2023-05-08 13:59:37 +0300

    add image and improve design of thank you page

--- mail.php
+++ mail.php
@@ -1,3 +1,11 @@
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="/dist/output.css" rel="stylesheet">
+  <script src="/src/file.js"></script>
+  <title>contact form</title>
+</head>
+
 <?php
   $message_sent = false;
   if(isset($_POST['email']) && $_POST['email'] != '') {
@@ -7,7 +15,8 @@
       $email = $_POST['email'];
       $subject = $_POST['subject'];
       $message = $_POST['message'];
-      $from = 'info2@amalibary-establishment.sa';
+      $from = 'info@amalibary-establishment.sa';
+      $customerSubject = "Thank you for contacting us";
 
       $headers  = 'MIME-Version: 1.0' . "\r\n";
       $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
@@ -23,11 +32,13 @@
   
       $body .= "From: ".$name. "<br>";
       $body .= "Email: ".$email. "<br>";
+      $body .= "Subject: ".$subject. "<br>";
       $body .= "Message: ".$message. "<br>";
+
   
       mail($recipient, $subject, $body, $headers)
       or die("Error!");
-      mail($recipient2, $subject, $body, $headers)
+      mail($recipient2, $customerSubject, $body, $headers)
       or die("Error!");
 
       $message_sent = true;
@@ -41,10 +52,14 @@
 <?php 
 if($message_sent):
 ?>
-    <div>
-    <h1 style="font-size:30px">Thank you for contact us</h1>
-    <button><a href="/index.html">Back to Contact form page</a></button>
-    </div>
+
+<div class="flex flex-col justify-center text-white items-center mx-auto bg-gradient-to-r from-black-light from-60% to-gray min-h-screen min-w-screen">
+        <img src="/src/img/check.png" width="150px" alt="">
+        <h1 class=" p-3 text-3xl">Thank you for contact us</h1>
+        <p>Your submission has been received.</p>
+        <button class="p-5 border  border-orange-500 w-1/4 mx-auto mt-5 hover:bg-gradient-to-r hover:from-orange-500 hover:to-orange-           800"><a href="/index.html">Back to Contact form page</a></button>
+        </div>
+
 <?php
 esle:
 ?>
